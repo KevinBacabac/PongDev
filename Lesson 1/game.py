bab618d685d4ceb56f7e2a247998f395eaaba517
Kevin <11673634+KevinBacabac@users.noreply.github.com>
2020-05-20 15:27:13 -0400

    Lesson 1

---
+++ Lesson 1/game.py
@@ -0,0 +1,35 @@
+import pygame  # Module imports
+import sys
+
+# Initialize pygame before use
+pygame.init()
+
+# Pick size of window
+SIZE = (800, 600)
+
+# Initialize the window
+window = pygame.display.set_mode(SIZE)
+
+# Control the frame rate
+FPS = 30
+# A special variable that will pause each frame
+clock = pygame.time.Clock()
+
+# Infinite loop
+while True:
+    # Check all pygame events
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit(0)
+
+    # Physics will go here
+
+    # Drawing
+    window.fill(pygame.Color("Black"))
+
+    # Show the new frame
+    pygame.display.update()
+
+    # Pause
+    clock.tick(FPS)
